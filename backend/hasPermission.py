6efcceaf9517bc44ef07837a795cb36ce5cadfd5
warleon <51892390+warleon@users.noreply.github.com>
2025-11-16 17:42:00 -0500

    Update hasPermission.py

--- backend/hasPermission.py
+++ backend/hasPermission.py
@@ -105,20 +105,12 @@ def lambda_handler(event, context):
 
 
 def generate_policy(principal_id, effect, resource):
-    """
-    Generates an IAM policy document for API Gateway.
-    """
-    auth_response = {}
-    auth_response["principalId"] = principal_id
-    auth_response["headers"] = CORS_HEADERS
-
-    if effect and resource:
-        policy_document = {
+    return {
+        "principalId": principal_id,
+        "policyDocument": {
             "Version": "2012-10-17",
             "Statement": [
                 {"Action": "execute-api:Invoke", "Effect": effect, "Resource": resource}
             ],
-        }
-        auth_response["policyDocument"] = policy_document
-
-    return auth_response
+        },
+    }
